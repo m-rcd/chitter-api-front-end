e233c5e01e8f2340fe65ba378d95d581018c807e
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-26 17:12:27 +0000

    Add eslint

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p> Chitter </p>
-
         </header>
       </div>
     );
   }
 }
-
-export default App;
